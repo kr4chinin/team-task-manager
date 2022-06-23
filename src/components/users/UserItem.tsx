5ee127dd4754bef87b677467da7295039812a101
Ilya Kruchinin <kr4chinin@mail.ru>
2022-06-23 21:10:22 +0300

    profile pictures and minor css improvements

--- src/components/users/UserItem.tsx
+++ src/components/users/UserItem.tsx
@@ -3,27 +3,27 @@ import { IUser } from '../../interfaces'
 import cl from './styles/UserItem.module.css'
 
 interface UserItemProps {
-	user: IUser
+    user: IUser
 }
 
 const UserItem: FC<UserItemProps> = ({ user }) => {
-	return (
-		<div className={cl.container}>
-			<div className={cl.avatar}></div>
-			<div className={cl['user-info']}>
-				<p id={cl.name}>{user.name}</p>
-				<p id={cl.email}>{user.email}</p>
-			</div>
-			<div>
-				<p id={cl.tasks}>
-					💻 Tasks: <span>10</span>
-				</p>
-				<p id={cl.completed}>
-					Completed: <span>5</span>
-				</p>
-			</div>
-		</div>
-	)
+    return (
+        <div className={cl.container}>
+            <img className={cl.avatar} alt="User profile" src={`https://picsum.photos/200?random=${user.id}`} />
+            <div className={cl['user-info']}>
+                <p id={cl.name}>{user.name}</p>
+                <p id={cl.email}>{user.email}</p>
+            </div>
+            <div>
+                <p id={cl.tasks}>
+                    💻 Tasks: <span>10</span>
+                </p>
+                <p id={cl.completed}>
+                    Completed: <span>5</span>
+                </p>
+            </div>
+        </div>
+    )
 }
 
 export default UserItem
