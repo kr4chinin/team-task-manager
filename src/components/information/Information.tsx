5bd39512969ee0120a355d167418496d4658c2f1
Ilya Kruchinin <kr4chinin@mail.ru>
2022-06-24 17:14:01 +0300

    created information page

--- src/components/information/Information.tsx
+++ src/components/information/Information.tsx
@@ -2,17 +2,36 @@ import cl from './Information.module.css'
 
 const Information = () => {
 	return (
-		<div className={cl.container}>
-			<h1>Information</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi dolorum
-				voluptate ex cumque vitae suscipit cupiditate modi architecto obcaecati
-				distinctio sapiente nisi quod quidem placeat exercitationem velit illum
-				ab, quaerat nemo ipsa voluptatum iure voluptates esse. Labore autem
-				placeat fugiat ipsam ducimus dolorem provident deleniti sequi at!
-				Maiores, reprehenderit hic?
-			</p>
-		</div>
+		<article className={cl.container}>
+			<h1 className={cl.title}>About Team Task Manager ⤵️</h1>
+			<div className={cl.information}>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam veniam
+				aliquam ea! Voluptatibus repellendus dolorum deleniti, incidunt
+				laudantium, non corporis aperiam veritatis sit eveniet aliquam
+				voluptatem aspernatur dolorem officiis architecto dolores magnam modi
+				hic sint saepe beatae ipsa nesciunt, impedit laboriosam. Vero quia
+				fugiat non veniam necessitatibus rem voluptatibus magni corporis eveniet
+				et laborum qui odio repudiandae aperiam vitae, dolores quisquam maiores,
+				accusamus ipsa. Dolore repellat inventore, maiores doloremque pariatur
+				voluptatum ea reiciendis voluptatem tempora beatae unde eius? Ut quas
+				voluptatem perspiciatis officia magni facilis nobis perferendis
+				voluptates voluptatum vero cupiditate similique voluptate ratione
+				aliquid, sapiente vitae iste dolore repudiandae itaque dicta veniam modi
+				sint dolores nulla? Vero, aliquid fuga. Amet a repudiandae ipsa officia
+				aliquid molestias accusamus consequuntur quis. Aut nisi vel voluptate
+				consequuntur nam placeat dolorem deserunt modi odio quos blanditiis ad
+				sunt ullam iusto saepe repellendus, atque ex, quisquam maxime laborum
+				eum expedita molestias sed? Eum, quam modi. Dolores, quae non? Illum
+				iure possimus exercitationem, natus inventore fugit consequatur
+				blanditiis odio aliquam quibusdam, praesentium quia at enim excepturi.
+				Suscipit dicta, rerum possimus, veritatis necessitatibus placeat
+				corrupti veniam eius ea eos ratione quod iste, perferendis odit dolorum
+				ipsam saepe? Eaque dicta eum quos repellendus qui? Quaerat enim veniam
+				esse, deserunt inventore facilis? Error, fugiat. Nulla, maiores
+				accusamus perspiciatis maxime reiciendis ad voluptatum, at ipsa vitae
+				consectetur doloremque cum?
+			</div>
+		</article>
 	)
 }
 
