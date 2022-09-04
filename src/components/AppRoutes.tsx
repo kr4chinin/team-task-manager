da9773ecb4da8c50d159acdee1584d05583effe3
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-04 16:03:56 +0300

    added redirect from unknown routes to /

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Information from './information/Information'
 import TaskListPage from './tasks/TaskListPage'
 import UserListPage from './users/UserListPage'
@@ -9,6 +9,7 @@ const AppRoutes = () => {
 			<Route path="/" element={<UserListPage />} />
 			<Route path="/user-tasks/:id" element={<TaskListPage />} />
 			<Route path="/info" element={<Information />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
 		</Routes>
 	)
 }
